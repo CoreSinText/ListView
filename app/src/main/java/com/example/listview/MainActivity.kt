07c98560a6a75a8ad4550c215884eff03d3dc21d
CoreSinText <muhammadrazak4@gmail.com>
2022-01-27 01:31:40 +0700

    first commit

---
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.listview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf("Sumatera", "Jawa","Bali", "Sulawesi", "Kalimantan", "Maluku", "Papua", "Batam")
+
+        var mListView = findViewById<ListView>(R.id.userlist)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+        mListView.adapter = arrayAdapter
+        
+    }
+}
